a3c0d7c0864aca6770298bb56fd2e7c32bc49008
trainingcourses1989@gmail.com <trainingcourses1989@gmail.com>
2023-03-22 16:49:52 +0000

    commit

--- models/Product.sql
+++
@@ -1,7 +0,0 @@
-
-with State_Sales as
-(
-select STATE,sum(NA_SALES),sum(NA_PROFIT) from product
-group by 1
-)
-select * from State_Sales
--- models/Product_Sales.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='table') }}
-
-
-with State_Sales as
-(
-select top 10* from PROD.PRODUCT
-)
-
-select * from State_Sales
