ecd598e677c7839ba8a5cdf490de9a0c54fa0abc
trainingcourses1989@gmail.com <trainingcourses1989@gmail.com>
2023-03-24 16:39:17 +0000

    commit

--- models/Sample_Examples/Store_sales_Prod.sql
+++
@@ -1,6 +0,0 @@
-{{ config(materialized='table') }}
-with store_sales as
-(
-select SS_STORE_SK,SS_EXT_SALES_PRICE,SS_EXT_WHOLESALE_COST,SS_NET_PROFIT from Prod.Store_Sales_2023
-)
-select * from store_sales
