3ca39bd98d5e794e7ada0bcb550de292b6056c51
trainingcourses1989@gmail.com <trainingcourses1989@gmail.com>
2023-03-25 21:45:26 +0000

    commit

--- models/example/Model.py
+++ models/example/Model.py
@@ -1,12 +1,9 @@
 
-
 def model(dbt,session):
-
 dbt.config(
     materialized="table",
-    package=["Pandas"]
-    )
-
+    packages=["Pandas"]
+)
 df=dbt.ref('store_Sales_tbl_2023')
 
 return df
--- models/example/PythonModel.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd_lib
-
-def model(dbt,session):
-    
-{{ config(materialized='table') }}
-
-df=dbt.ref('store_Sales_tbl_2023')
-
-return df
